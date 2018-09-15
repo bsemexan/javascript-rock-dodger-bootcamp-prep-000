aa6b58643ded324c6c72689aac06a6bba07955ff
Berry  Semexan <bsemexan@gmail.com>
2018-09-15 17:24:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -115,6 +115,7 @@ function endGame() {
   ROCKS.forEach(function(rock) { rock.remove() })
 
   document.removeEventListener('keydown', moveDodger)
+  return alert('YOU LOSE!')
 }
 
 function moveDodger(e) {
