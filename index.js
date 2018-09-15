85490d3cc012ca2a3073a7eccca4090a0f5c3d5e
Berry  Semexan <bsemexan@gmail.com>
2018-09-15 17:34:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -165,7 +165,7 @@ function moveDodgerRight() {
    window.requestAnimationFrame(function() {
     const left = positionToInteger(DODGER.style.left)
 
-    if (left > 0) {
+    if (left < 360) {
       DODGER.style.left = `${left + 4}px`;
     }
   })
